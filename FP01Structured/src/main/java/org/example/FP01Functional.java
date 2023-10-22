e9837033a05b75a2c038fd40459788cc4423c5d2
김산해 <sanhae1996@gmail.com>
2023-10-22 20:57:57 +0900

    Improving Java Functional Program with filter

--- FP01Structured/src/main/java/org/example/FP01Functional.java
+++ FP01Structured/src/main/java/org/example/FP01Functional.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+        //printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+        printEvenNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
     }
 
 //    private static void print(int number) {
@@ -18,9 +19,16 @@ public class FP01Functional {
     }
 
 
+    private static boolean isEven(int number)
+    {
+        return number%2 == 0;
+    }
+
     private static void printEvenNumbersInListStructured(List<Integer> numbers) {
         // How to loop the numbers?
         numbers.stream()
+                //Filter - Only allow even numbers
+                .filter(FP01Functional::isEven)
                 .forEach(System.out::println); // Method Reference
     }
 }
