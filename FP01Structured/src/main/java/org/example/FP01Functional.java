6bc7679aa251fadb959698a55deb7238bce8eca5
김산해 <sanhae1996@gmail.com>
2023-10-24 20:17:14 +0900

    Using map in Functional Programs Exercises

--- FP01Structured/src/main/java/org/example/FP01Functional.java
+++ FP01Structured/src/main/java/org/example/FP01Functional.java
@@ -6,7 +6,9 @@ public class FP01Functional {
     public static void main(String[] args) {
         //printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
         //printEvenNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
-        printSquaresEvenNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+        //printSquaresEvenNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+        printCubesOddNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+
     }
 
 //    private static void print(int number) {
@@ -40,6 +42,14 @@ public class FP01Functional {
         numbers.stream()
                 .filter(number -> number % 2 == 0)
                 .map(number -> number * number)
+                .forEach(System.out::println);
+    }
+
+    private static void printCubesOddNumbersInListStructured(List<Integer> numbers)
+    {
+        numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .map(number -> number * number * number)
                 .forEach(System.out::println);
     }
 }
